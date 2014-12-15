41048301f9b18eaaa1ad96054017a4ee3551e2eb
zheshishei <asun0927@gmail.com>
2014-12-15 14:58:44 -0800

    Completed taxonomy

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.treasureChest = [];
+  this.canFly = true;
+  this.job = 'find pollen';
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+};
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+HoneyMakerBee.prototype.giveHoney = function() {
+  if(this.honeyPot > 0) {
+    this.honeyPot--;
+  }
+};
